9ca80cf9e61dddf6fd4bf1fbef3391d727d6f348
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-15 13:07:37 +0530

    Create ui_helpers.dart

---
+++ lib/ui/common/lib/ui/common/lib/ui/common/ui_helpers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// This file is for UI helper constants and functions, like spacing widgets.
+const Widget horizontalSpaceTiny = SizedBox(width: 5.0);
+const Widget horizontalSpaceSmall = SizedBox(width: 10.0);
+const Widget horizontalSpaceMedium = SizedBox(width: 25.0);
+
+const Widget verticalSpaceTiny = SizedBox(height: 5.0);
+const Widget verticalSpaceSmall = SizedBox(height: 10.0);
+const Widget verticalSpaceMedium = SizedBox(height: 25.0);
+const Widget verticalSpaceLarge = SizedBox(height: 50.0);
