97921df509de56d0de6b8ca713ada2d6368f0fb9
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-15 13:05:22 +0530

    Create app_colors.dart

---
+++ lib/ui/common/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kcPrimaryColor = Color(0xFF9147FF); // Lilac
+const Color kcSecondaryColor = Color(0xFF3D99E5); // Blue
+const Color kcGlowColor = Color(0xFF42F5B4); // Green Glow
+const Color kcBackgroundColor = Color(0xFF0D0D1A);
+const Color kcCardColor = Color(0x1AFFFFFF); // Glassmorphism card color
